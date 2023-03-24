de078ee9c9093009ad497296d5b115458a9d15f1
MaxChentx <ctx195467@163.com>
2023-03-24 16:32:13 +0800

    ignore this: com.chentx.tables.module01.Main

--- _Modules/src/main/java/com/chentx/tables/module01/Main.java
+++ _Modules/src/main/java/com/chentx/tables/module01/Main.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Logger;
 
 /**
  * @author MaxBrooks chentingxian195467@163.com
@@ -14,22 +15,6 @@ import java.sql.Statement;
 public class Main {
 
     public static void main(String[] args) {
-//        String[][] results;
-//
-//        DBConnector dbConnector = new DBConnector();
-//        dbConnector.setSql("SELECT * FROM inboundandoutboundtable");
-//
-//        results = dbConnector.getRecord();
-//        String[] tableHead = dbConnector.getColumnName();
-//        for (String s : tableHead) {
-//            System.out.print(s + " ");
-//        }
-//        for (int i = 0; i < results.length; i++){
-//            for (int j = 0; j < results[i].length; j++) {
-//                System.out.println(results[i][j] + "  ");
-//            }
-//            System.out.println();
-//        }
 
         Connection con;
         Statement sql;
@@ -43,10 +28,10 @@ public class Main {
         try {
             sql = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
 
-            rs = sql.executeQuery("SELECT * FROM inboundandoutboundtable");
+            rs = sql.executeQuery("SELECT * FROM inboundtable");
             rs.last();
             int max = rs.getRow();
-            System.out.println("the table has " + max + " records");
+            Logger.getGlobal().info("the table has " + max + " records");
 
             int[] a = GetRandomNumber.getRandomNumber(max, max);
             for (int i : a) {
@@ -55,27 +40,14 @@ public class Main {
                 String x = rs.getString(2);
                 String c = rs.getString(3);
                 String v = rs.getString(4);
-                int b = rs.getInt(5);
-//                String n = rs.getString(6);
-//                String m = rs.getString(7);
-//                String l = rs.getString(8);
-//                String k = rs.getString(9);
-//                String j = rs.getString(10);
-//                String h = rs.getString(11);
-                System.out.printf("%d\t", z);
-                System.out.printf("%s\t", x);
-                System.out.printf("%s\t", c);
-                System.out.printf("%s\t", v);
-                System.out.printf("%d\t\n", b);
+                String b = rs.getString(5);
+
+                Logger.getGlobal().info(z + "\t" + x + "\t" + c + "\t" + v + "\t" + b + "\n");
             }
             con.close();
 
-
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
-
-
     }
-
 }
